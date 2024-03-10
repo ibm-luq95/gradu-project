3993e6424ae3f04fdfb59e63b47e9ee8930a8bdf
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-03-10 14:40:25 +0300

    add home url

--- src/techweb_project/urls.py
+++ src/techweb_project/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+static_and_media_path_urls = static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", include("home.urls"), name="home"),
+    path("dashboard/", include("dashboard.urls"), name="dashboard"),
 ]
+urlpatterns += static_and_media_path_urls
+if settings.DEBUG:
+
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
+    urlpatterns.append(path("admin/", admin.site.urls))
+    urlpatterns.append(path("admin/doc/", include("django.contrib.admindocs.urls")))
+else:
+    urlpatterns.append(path("secret/", admin.site.urls))
