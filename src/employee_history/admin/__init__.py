580a2f8606710b98577ab0a5b451af8c826c4e09
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-04-27 18:41:51 +0300

    fix: delete employee history app

--- src/employee_history/admin/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-#
-from .employee_history import EmployeeHistoryAdmin
-
-__all__ = ["EmployeeHistoryAdmin"]
--- src/employee_history/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-#
-from .employee_history import EmployeeHistory
-
-
-__all__ = ["EmployeeHistory"]
