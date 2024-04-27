5f35bb57020822b17baee0f24b3e2f8f7bd3c4a5
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-04-27 18:43:49 +0300

    fix: remove not needed fields

--- src/employee/models/employee.py
+++ src/employee/models/employee.py
@@ -8,26 +8,10 @@ from core.models.mixins.base_model import BaseModelMixin
 
 class Employee(BaseModelMixin):
     user = models.OneToOneField("techweb_user.TechWebUser", on_delete=models.CASCADE)
-    employee_id = models.PositiveBigIntegerField(_("ID"))
-    token = models.CharField(_("Token"), max_length=255, null=True, blank=True)
 
     class Meta(BaseModelMixin.Meta):
         verbose_name = _("Employee")
         verbose_name_plural = _("Employees")
         indexes = [
             models.Index(name="employee_user_idx", fields=["user"]),
-            models.Index(name="employee_id_idx", fields=["employee_id"]),
-            models.Index(name="employee_token_idx", fields=["token"]),
-        ]
-        constraints = [
-            models.UniqueConstraint(
-                fields=["employee_id"],
-                name="unique_employee_id_unique_constraint",
-                condition=Q(employee_id__isnull=False),
-            ),
-            models.UniqueConstraint(
-                fields=["token"],
-                name="unique_employee_token_unique_constraint",
-                condition=Q(token__isnull=False),
-            ),
         ]
