1e2210d1c0a7325b217c57219004e01facf3e071
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-05-15 21:37:29 +0300

    fix: no ready yet

--- src/dashboard/views/api.py
+++ src/dashboard/views/api.py
@@ -27,25 +27,56 @@ class DashboardAPIView(APIView):
             classification_chart_data = []
             departments_chart_data = []
             for d_slug in SurveyDepartmentsTypeChoices.choices:
-                departments_chart_data.append(
-                    {
-                        "slug": d_slug[0],
-                        "count": Survey.objects.filter(department__in=[d_slug[0]]).count(),
-                        "label": d_slug[1],
-                    }
-                )
+                departments_chart_data.append({
+                    "slug": d_slug[0],
+                    "count": Survey.objects.filter(department__in=[d_slug[0]]).count(),
+                    "label": d_slug[1],
+                })
             all_classifications = Classification.objects.all()
             for classification in all_classifications:
-                classification_chart_data.append(
-                    {
-                        "slug": classification.slug,
-                        "label": classification.label,
-                        "surveys_count": classification.surveys.count(),
-                    }
-                )
+                classification_chart_data.append({
+                    "slug": classification.slug,
+                    "label": classification.label,
+                    "surveys_count": classification.surveys.count(),
+                })
             response["classification_chart_data"] = classification_chart_data
             response["departments_chart_data"] = departments_chart_data
             response["quarter_chart_data"] = quarter_chart_data
+            all_departments = []
+            for dep in SurveyDepartmentsTypeChoices.choices:
+                all_departments.append(dep[1])
+            response["all_departments"] = all_departments
+            months_idx = list(range(1, 13, 1))
+            DebuggingPrint.pprint(months_idx)
+            all_surveys = Survey.objects.all()
+            # for a in all_surveys:
+            # DebuggingPrint.pprint(a.created_at)
+            # DebuggingPrint.pprint(a.created_at.month)
+            department_data = []
+            for midx in months_idx:
+                DebuggingPrint.log(midx)
+                alls = Survey.objects.filter(created_at__month=midx)
+                DebuggingPrint.print(alls)
+                DebuggingPrint.print(alls.count())
+                for department in SurveyDepartmentsTypeChoices.choices:
+                    department_code = department[0]
+                    DebuggingPrint.pprint(department_code)
+                    department_qs = alls.filter(department=department_code)
+                    DebuggingPrint.pprint(department_qs)
+                department_data.append(
+                    {"month_idx": midx, "month_abbr": calendar.month_abbr[midx]}
+                )
+                DebuggingPrint.rule()
+            # for department in SurveyDepartmentsTypeChoices.choices:
+            #     DebuggingPrint.pprint(department)
+            #     for midx in months_idx:
+            #         # DebuggingPrint.pprint(midx)
+            #         q = Survey.objects.filter(
+            #             department=department[0]
+            #         )
+            #         DebuggingPrint.pprint(q.first().created_at.month)
+            #     # DebuggingPrint.pprint(q)
+            #     DebuggingPrint.rule()
             return Response(data=response, status=status.HTTP_200_OK)
         except Exception as ex:
             print(traceback.format_exc())
