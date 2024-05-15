cd780f5989560b43ea6aa613e147333111a83da0
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-05-15 21:35:09 +0300

    new: create new constants

--- src/core/constants/survey.py
+++ src/core/constants/survey.py
@@ -11,3 +11,8 @@ CLSF_HARDWARE_DEVELOPMENT = "hardware_development"
 CLSF_IT_SERVICES = "it_services"
 CLSF_CYBER_SECURITY = "cyber_security"
 CLSF_OTHER = "other"
+
+
+EMP_STATUS_FULL_TIME = "full_time"
+EMP_STATUS_PART_TIME = "part_time"
+EMP_STATUS_CONTRACT = "contract"
