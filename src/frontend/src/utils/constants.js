09d55d2fe8877704f154180b8c88fb88ff5ad771
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-05-15 21:38:13 +0300

    new: new contant PRELINECSSLOADERHTML

--- src/frontend/src/utils/constants.js
+++ src/frontend/src/utils/constants.js
@@ -1,4 +1,7 @@
 const CSRFINPUTNAME = "csrfmiddlewaretoken";
 const baseUrl = window.location.origin;
 const FETCHURLNAMEURL = new URL(process.env.FETCHURLNAMEURL, baseUrl);
-export { CSRFINPUTNAME, FETCHURLNAMEURL };
+const PRELINECSSLOADERHTML = `<div class="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
+<span class="sr-only">Loading...</span>
+</div>`;
+export { CSRFINPUTNAME, FETCHURLNAMEURL, PRELINECSSLOADERHTML };
