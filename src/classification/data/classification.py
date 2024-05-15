f4404a8baacc0dbb12df63913498758ed2d85d7b
Ibrahim Luqman <ibm.jnk@yandex.com>
2024-05-15 21:33:55 +0300

    fix: add classification_number

--- src/classification/data/classification.py
+++ src/classification/data/classification.py
@@ -8,8 +8,10 @@ MAIN_CLASSIFICATIONS: List[dict[str, Union[str, int, Tuple[str], List[str]]]] = 
         "label": _("Healthy"),
         "image_name": "Healthy.jpg",
         "min_value": 33,
+        "classification_number": 2,
         "description": _(
-            "Feeling generally positive and capable of handling daily stressors effectively."
+            "Feeling generally positive and capable of handling daily stressors"
+            " effectively."
         ),
         "short_advice": _(
             "Congratulations on maintaining a good balance! To continue thriving"
@@ -18,10 +20,12 @@ MAIN_CLASSIFICATIONS: List[dict[str, Union[str, int, Tuple[str], List[str]]]] = 
         "recommendations": (
             _("Remember the importance of regular breaks, even on busy days."),
             _(
-                "Engaging in activities outside of work, whether it's a hobby or exercise, can further enhance your mental resilience."
+                "Engaging in activities outside of work, whether it's a hobby or exercise,"
+                " can further enhance your mental resilience."
             ),
             _(
-                "Stay connected with your support network and don't hesitate to seek help if you ever feel overwhelmed."
+                "Stay connected with your support network and don't hesitate to seek help"
+                " if you ever feel overwhelmed."
             ),
         ),
     },
@@ -29,61 +33,77 @@ MAIN_CLASSIFICATIONS: List[dict[str, Union[str, int, Tuple[str], List[str]]]] = 
         "label": _("Mildly Stressed"),
         "image_name": "Mildly Stressed.png",
         "min_value": 39,
+        "classification_number": 4,
         "conclusion_advice": _("Remember, stay safe."),
         "description": _(
-            "Coping well but occasionally feel overwhelmed or anxious due to manageable stressors in your life."
+            "Coping well but occasionally feel overwhelmed or anxious due to manageable"
+            " stressors in your life."
         ),
         "short_advice": _(
-            "It's great that you're managing well overall, but there are signs of stress to address."
+            "It's great that you're managing well overall, but there are signs of stress"
+            " to address."
         ),
         "recommendations": (
             _(
-                "Consider refining your work-life balance; perhaps delegating tasks or setting more realistic deadlines could help"
+                "Consider refining your work-life balance; perhaps delegating tasks or"
+                " setting more realistic deadlines could help"
             ),
             _(
-                "Mindfulness techniques, like meditation or deep breathing exercises, can be highly effective."
+                "Mindfulness techniques, like meditation or deep breathing exercises, can"
+                " be highly effective."
             ),
             _(
-                "Also, ensure you're getting adequate rest and using your time off to truly disconnect and recharge."
+                "Also, ensure you're getting adequate rest and using your time off to"
+                " truly disconnect and recharge."
             ),
         ),
     },
     {
         "label": _("Highly Stressed"),
         "min_value": 46,
+        "classification_number": 3,
         "image_name": "Highly Stressed.webp",
         "short_advice": _(
-            "Your well-being is crucial, and it's important to take steps to reduce your stress levels."
+            "Your well-being is crucial, and it's important to take steps to reduce your"
+            " stress levels."
         ),
         "description": _("Feeling overwhelmed, drained, and unable to cope."),
         "conclusion_advice": _("Remember, we are always here when you need us!"),
         "recommendations": (
             _("Prioritize tasks and discuss workload management with your supervisor."),
             _(
-                "Regular physical activity and a healthy diet can significantly impact your stress levels."
+                "Regular physical activity and a healthy diet can significantly impact"
+                " your stress levels."
             ),
             _(
-                "It might also be beneficial to explore stress management resources or speak with a mental health professional for personalized strategies."
+                "It might also be beneficial to explore stress management resources or"
+                " speak with a mental health professional for personalized strategies."
             ),
         ),
     },
     {
         "label": _("Burned Out"),
         "image_name": "BurnOut.png",
+        "classification_number": 1,
         "min_value": 50,
         "short_advice": _(
-            "We understand you're experiencing burnout, and I want to offer some concise advice."
+            "We understand you're experiencing burnout, and I want to offer some concise"
+            " advice."
         ),
         "description": _(
-            "Experiencing extreme mental and physical exhaustion,  feeling detached, and lacking motivation, typically due to prolonged stress."
+            "Experiencing extreme mental and physical exhaustion,  feeling detached, and"
+            " lacking motivation, typically due to prolonged stress."
         ),
         "recommendations": (
             _("Please consider taking a well-deserved break to rest and recharge."),
             _(
-                "It's important to set clear boundaries around your work hours and prioritize self-care, including adequate sleep, exercise, and healthy eating."
+                "It's important to set clear boundaries around your work hours and"
+                " prioritize self-care, including adequate sleep, exercise, and healthy"
+                " eating."
             ),
             _(
-                "Don't hesitate to seek support from management, HR, or a mental health professional."
+                "Don't hesitate to seek support from management, HR, or a mental health"
+                " professional."
             ),
         ),
         "conclusion_advice": _("Remember, your well-being is paramount."),
